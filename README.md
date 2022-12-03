005214c5b0976017f36414924197b14ea097a7d0
Jeff Pignataro <jeffp2662@gmail.com>
2022-12-03 11:11:39 -0700

    Demo cli project with flags and subcommands

--- README.md
+++ README.md
@@ -6,32 +6,44 @@ _All these commands assume you are running them from the root of the project._
 
 Build everything
 
-```
+```bash
 bazel build //...
 ```
 
 Build specific thing
 
-```
+```bash
 bazel build //cmd/cli-demo:cli-demo
 ```
 
 Run something
 
-```
+```bash
 bazel run //cmd/cli-demo:cli-demo
 ```
 
 Update `BUILD.bazel` files
 
-```
+```bash
 bazel run //:gazelle
 ```
 
 Update `WORKSPACE` file for new dependencies
 
-```
+```bash
 bazel run //:gazelle -- update-repos -from_file go.mod
 ```
 
 _Note if you `go mod tidy` it's always a good idea to `update-repos` at the same time._
+
+## Testing methods
+
+Running CLI commands which were just built is a little awkward as they live in the `bazel-bin` folder and are buried under a large folder structure. To make this is a little easier you can create a simple `symbolic link` under the `/usr/local/bin` folder (which should already be in the `PATH`) making it easy to rerun the command every build.
+
+```bash
+sudo ln -s ~/Repos/golang/bazel-bin/cmd/cli-demo/cli-demo_/cli-demo /usr/local/bin/cli-demo
+
+which cli-demo # should return /usr/local/bin/cli-demo
+```
+
+Then you can easily run `cli-demo` to execute the command after each `bazel build \\...`
--- cmd/cli-demo/main.go
+++ cmd/cli-demo/main.go
@@ -4,18 +4,86 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"time"
 
-	"github.com/urfave/cli"
+	"github.com/urfave/cli/v2"
 )
+
+var flag1Value string
 
 func main() {
 	app := &cli.App{
-		Name:  "boom",
-		Usage: "make an explosive entrance",
-		Action: func(*cli.Context) error {
-			fmt.Println("boom! I say!")
+		Name:           "demo",
+		HelpName:       "",
+		Usage:          "A sample CLI application",
+		UsageText:      "",
+		ArgsUsage:      "",
+		Version:        "",
+		Description:    "",
+		DefaultCommand: "",
+		Commands: []*cli.Command{
+			{
+				Name:    "sub",
+				Aliases: []string{"s"},
+				Usage:   "demo sub command",
+				Action: func(c *cli.Context) error {
+					fmt.Printf("Sub command: %s", c.Args().First())
+					return nil
+				},
+			},
+		},
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:        "flag1",
+				Usage:       "This is a demo flag",
+				Destination: &flag1Value,
+			},
+		},
+		EnableBashCompletion: true,
+		HideHelp:             false,
+		HideHelpCommand:      false,
+		HideVersion:          false,
+		BashComplete: func(*cli.Context) {
+		},
+		Before: func(*cli.Context) error {
 			return nil
 		},
+		After: func(*cli.Context) error {
+			return nil
+		},
+		Action: func(c *cli.Context) error {
+			if flag1Value == "" {
+				fmt.Printf("Hello %s", c.Args().Get(0))
+			} else {
+				fmt.Printf("%s", flag1Value)
+			}
+			return nil
+		},
+		CommandNotFound: func(*cli.Context, string) {
+		},
+		OnUsageError: func(cCtx *cli.Context, err error, isSubcommand bool) error {
+			return nil
+		},
+		InvalidFlagAccessHandler: func(*cli.Context, string) {
+		},
+		Compiled:  time.Time{},
+		Authors:   []*cli.Author{},
+		Copyright: "",
+		Reader:    nil,
+		Writer:    nil,
+		ErrWriter: nil,
+		ExitErrHandler: func(cCtx *cli.Context, err error) {
+		},
+		Metadata: map[string]interface{}{},
+		ExtraInfo: func() map[string]string {
+			return nil
+		},
+		CustomAppHelpTemplate:  "",
+		SliceFlagSeparator:     "",
+		UseShortOptionHandling: false,
+		Suggest:                false,
+		AllowExtFlags:          false,
+		SkipFlagParsing:        false,
 	}
 
 	if err := app.Run(os.Args); err != nil {
