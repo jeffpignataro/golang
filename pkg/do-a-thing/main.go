9bb928564519a7f103a8f4941aa49439837d819d
Jeff Pignataro <jeffp2662@gmail.com>
2023-03-18 10:21:20 -0600

    Updating based on v0.0.2

--- pkg/do-a-thing/main.go
+++ pkg/do-a-thing/main.go
@@ -3,6 +3,6 @@ package doathing
 import "fmt"
 
 func Doathing(s string) string {
-	fmt.Print("Do another thing in v0.0.4\n")
+	fmt.Print("Do another thing in v0.0.2\n")
 	return fmt.Sprintf("Doing a thing...%s", s)
 }
