682eb8280362889e04099d24473fa8a80dd80f65
Jeff Pignataro <jeffp2662@gmail.com>
2023-09-04 10:49:34 -0600

    I'm confused but it builds...

--- cmd/proto/main.go
+++ cmd/proto/main.go
@@ -3,19 +3,19 @@ package main
 import (
 	"fmt"
 
-	"github.com/jeffpignataro/golang/cmd/proto/models/persons/person"
+	p "github.com/jeffpignataro/golang/cmd/proto/models/persons/person"
 )
 
 func main() {
 	// var a addressbook.AddressBook
-	p := person.Person{
+	p := p.Person{
 		Name:  "My Name",
 		Id:    0,
 		Email: "my@email.com",
-		Phones: []*person.Person_PhoneNumber{
-			&person.Person_PhoneNumber{
+		Phones: []*p.Person_PhoneNumber{
+			{
 				Number: "123-456-7890",
-				Type:   person.Person_HOME,
+				Type:   p.Person_HOME,
 			},
 		},
 	}
